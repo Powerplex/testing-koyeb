5e0ff716dddb243a08e67bd152b6716169ab4a8f
Powerplex <julien.bonnin.dev@gmail.com>
2026-01-19 15:47:58 +0100

    Initial Astro site

---
+++ src/components/Banner.tsx
@@ -0,0 +1,7 @@
+export const Banner = () => {
+  return (
+    <div className="bg-blue-800 text-white text-3xl p-4 shadow-lg">
+      <h1>Testing Koyeb (Astro + React + TailwindCSS)</h1>
+    </div>
+  );
+};
